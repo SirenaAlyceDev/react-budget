ef6964554ed52d815920afc7cb643b4a53158f48
Sirena Gandy <sirena@sirenaalyce.com>
2021-01-16 09:48:15 -0500

    added redirect to CTA button modal submit

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -12,12 +12,12 @@ import {
   Modal,
   ModalBody,
   ModalHeader,
-  ModalFooter
+  ModalFooter,
 } from "reactstrap";
 import classnames from "classnames";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
-import { Redirect } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 const Home = (props) => {
   const [activeTab, setActivetab] = useState("1");
@@ -27,145 +27,148 @@ const Home = (props) => {
 
   const [modal, isModalOpen] = useState(false);
   const toggleModal = () => isModalOpen(!modal);
-  
+
+  const history = useHistory();
+  const handleClick = () => history.push("/budgetstyle");
 
   return (
     <React.Fragment>
-      <Container fluid className="d-flex align-items-center mainContainer"> 
-          <div className="row w-100 align-items-center">
-            <div className="col text-right headerDiv">
-              <p className="pre-header">MANAGING YOUR MONEY YOUR WAY</p>
-              <h1 className="heading">
-                BUDGETING YOUR WAY
-                <br /> TO FINANICAL <br /> PEACE
-              </h1>
-              <h3 className="subheading">
-                KEEP YOUR EYES ON THE PRIZE AND CREATE A BUDGET PLAN <br /> TO
-                CHANGE YOUR FUTURE
-              </h3>
-              <Button size="sm" className="ctahome" onClick={toggleModal}>
-                Start Budgeting
-              </Button>
-            </div>
-            <div className="col text-center homeImageDiv">
-              <img
-                className="home-image"
-                src="/assets/images/pexels-ketut-subiyanto-4350108.jpg"
-                height="400"
-                width="300"
-                alt="home-img"
-              />
-            </div>
-          </div>   
+      <Container fluid className="d-flex align-items-center mainContainer">
+        <div className="row w-100 align-items-center">
+          <div className="col text-right headerDiv">
+            <p className="pre-header">MANAGING YOUR MONEY YOUR WAY</p>
+            <h1 className="heading">
+              BUDGETING YOUR WAY
+              <br /> TO FINANICAL <br /> PEACE
+            </h1>
+            <h3 className="subheading">
+              KEEP YOUR EYES ON THE PRIZE AND CREATE A BUDGET PLAN <br /> TO
+              CHANGE YOUR FUTURE
+            </h3>
+            <Button size="sm" className="ctahome" onClick={toggleModal}>
+              Start Budgeting
+            </Button>
+          </div>
+          <div className="col text-center homeImageDiv">
+            <img
+              className="home-image"
+              src="/assets/images/pexels-ketut-subiyanto-4350108.jpg"
+              height="400"
+              width="300"
+              alt="home-img"
+            />
+          </div>
+        </div>
       </Container>
 
       <Modal isOpen={modal} toggle={toggleModal}>
-          <ModalHeader toggle={toggleModal} className="text-center">Let's get started!</ModalHeader>
-          <ModalBody>Next stop...financial peace</ModalBody>
-          <ModalFooter>
-            <Button>Submit</Button> <Button>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+        <ModalHeader toggle={toggleModal} className="text-center">
+          Let's get started!
+        </ModalHeader>
+        <ModalBody>Next stop...financial peace</ModalBody>
+        <ModalFooter>
+          <Button onClick={handleClick}>Submit</Button> <Button>Cancel</Button>
+        </ModalFooter>
+      </Modal>
 
       <Container fluid className="tabscontainer fixed-bottom">
-      <div className="row w-100">
-            <div className="col features">
-              <h2 className="featuresheader">FEATURES</h2>
-            </div>
+        <div className="row w-100">
+          <div className="col features">
+            <h2 className="featuresheader">FEATURES</h2>
           </div>
-          <div className="row w-100">
-            <div className="col">
-              <Nav tabs>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: activeTab === "1" })}
-                    onClick={() => {
-                      toggle("1");
-                    }}
-                  >
-                    Eyes on the Prize
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: activeTab === "2" })}
-                    onClick={() => {
-                      toggle("2");
-                    }}
-                  >
-                    Reduce Debt
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: activeTab === "3" })}
-                    onClick={() => {
-                      toggle("3");
-                    }}
-                  >
-                    Organize Spending & Savings
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={classnames({ active: activeTab === "4" })}
-                    onClick={() => {
-                      toggle("4");
-                    }}
-                  >
-                    Kill Bad Habits & Make Better Choices
-                  </NavLink>
-                </NavItem>
-              </Nav>
-              <TabContent activeTab={activeTab} >
-                <TabPane tabId="1">
-                  <Row>
-                    <Col>
-                      <p>
-                        Donut jelly-o pastry toffee. Chocolate cake pudding
-                        jelly-o. Cheesecake halvah soufflé bonbon. Cupcake
-                        dessert pastry pastry lemon drops dessert apple pie pie.
-                      </p>
-                    </Col>
-                  </Row>
-                </TabPane>
-                <TabPane tabId="2">
-                  <Row>
-                    <Col>
-                      <p>
-                        Jelly-o danish gummies brownie cake sugar plum bonbon
-                        bear claw. Apple pie candy canes chupa chups ice cream
-                        pie chocolate cake. Lemon drops apple pie macaroon
-                        powder.
-                      </p>
-                    </Col>
-                  </Row>
-                </TabPane>
-                <TabPane tabId="3">
-                  <Row>
-                    <Col>
-                      <p>
-                        Cake topping biscuit. Cupcake toffee cheesecake danish
-                        biscuit gummi bears croissant tiramisu. Candy canes
-                        chocolate cake candy canes.
-                      </p>
-                    </Col>
-                  </Row>
-                </TabPane>
-                <TabPane tabId="4">
-                  <Row>
-                    <Col>
-                      <p>
-                        Macaroon sweet biscuit lollipop pie jujubes tart.
-                        Chocolate chocolate cake cotton candy. Tiramisu cotton
-                        candy soufflé.
-                      </p>
-                    </Col>
-                  </Row>
-                </TabPane>
-              </TabContent>
-            </div>
+        </div>
+        <div className="row w-100">
+          <div className="col">
+            <Nav tabs>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "1" })}
+                  onClick={() => {
+                    toggle("1");
+                  }}
+                >
+                  Eyes on the Prize
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "2" })}
+                  onClick={() => {
+                    toggle("2");
+                  }}
+                >
+                  Reduce Debt
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "3" })}
+                  onClick={() => {
+                    toggle("3");
+                  }}
+                >
+                  Organize Spending & Savings
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className={classnames({ active: activeTab === "4" })}
+                  onClick={() => {
+                    toggle("4");
+                  }}
+                >
+                  Kill Bad Habits & Make Better Choices
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+              <TabPane tabId="1">
+                <Row>
+                  <Col>
+                    <p>
+                      Donut jelly-o pastry toffee. Chocolate cake pudding
+                      jelly-o. Cheesecake halvah soufflé bonbon. Cupcake dessert
+                      pastry pastry lemon drops dessert apple pie pie.
+                    </p>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="2">
+                <Row>
+                  <Col>
+                    <p>
+                      Jelly-o danish gummies brownie cake sugar plum bonbon bear
+                      claw. Apple pie candy canes chupa chups ice cream pie
+                      chocolate cake. Lemon drops apple pie macaroon powder.
+                    </p>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="3">
+                <Row>
+                  <Col>
+                    <p>
+                      Cake topping biscuit. Cupcake toffee cheesecake danish
+                      biscuit gummi bears croissant tiramisu. Candy canes
+                      chocolate cake candy canes.
+                    </p>
+                  </Col>
+                </Row>
+              </TabPane>
+              <TabPane tabId="4">
+                <Row>
+                  <Col>
+                    <p>
+                      Macaroon sweet biscuit lollipop pie jujubes tart.
+                      Chocolate chocolate cake cotton candy. Tiramisu cotton
+                      candy soufflé.
+                    </p>
+                  </Col>
+                </Row>
+              </TabPane>
+            </TabContent>
           </div>
+        </div>
       </Container>
     </React.Fragment>
   );
