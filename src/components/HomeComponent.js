da8a1ec865f0dea6942d604b04cb904185105119
Sirena Gandy <sirena@sirenaalyce.com>
2021-03-14 20:26:12 -0400

    replaced submit alert with console.log

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -63,7 +63,7 @@ const Home = () => {
     },
     validate,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      console.log(JSON.stringify(values, null, 2));
       history.push("/budgetstyle");
     },
   });
